f8a701174b6e37e00e3a7e716dcdb2ce9615bfff
Dirk Dodson <dirk@hotpointapp.com>
2022-04-06 19:48:43 -0700

    Library Updates

--- main.cpp
+++ main.cpp
@@ -4,12 +4,10 @@
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
-#include <utility>
-#include <numeric>
 
-#include "./library/myFFmpegConversion.hpp"
-#include "./library/metadataFunction.hpp"
-#include "./library/dnaGenerator.hpp"
+#include "./library/my_ffmpeg_conversion.hpp"
+#include "./library/metadata_function.hpp"
+#include "./library/dna_generator.hpp"
 #include "./library/utils.hpp"
 
 using namespace std;
@@ -18,22 +16,25 @@ int main(int argc, char *argv[])
 {
     if(argc>=2){
         srand(time(NULL));
-        const int scale = 1024;
+        const int scale = 240;
         const string outputFormat = "gif";
-        const bool randomized = true;
+        const bool randomized = false;
         const bool unique = true;
-        const bool shuffleOutput = true;
-        vector<int> collectionSize = {25,25,150,150,775,775};
+        const bool shuffleOutput = false;
+        vector<int> collectionSize = {25,25,150,150,775,775,775,775,775,775};
         const vector<vector<string>> layerDir = 
         {
-    //GOAT   
     {"BACKGROUND_RARE", "BULLDOG_Glasses", "BULLDOG_Pants", "BULLDOG_Arms", "BULLDOG_Shoes", "BULLDOG_Shirt", "BULLDOG_Necklace", "BULLDOG_Move", "BULLDOG_Ball","GOAT_Range", "GOAT_XP"},
     {"BACKGROUND_RARE", "SHAM_GOD_Glasses", "SHAM_GOD_Pants", "SHAM_GOD_Arms", "SHAM_GOD_Shoes", "SHAM_GOD_Shirt", "SHAM_GOD_Necklace", "SHAM_GOD_Move", "SHAM_GOD_Ball","GOAT_Range", "GOAT_XP"},
     {"BACKGROUND", "BETWEEN_BEHIND_THE_BACK_Glasses", "BETWEEN_BEHIND_THE_BACK_Pants", "BETWEEN_BEHIND_THE_BACK_Arms", "BETWEEN_BEHIND_THE_BACK_Shoes", "BETWEEN_BEHIND_THE_BACK_Shirt", "BETWEEN_BEHIND_THE_BACK_Necklace", "BETWEEN_BEHIND_THE_BACK_Move", "BETWEEN_BEHIND_THE_BACK_Ball", "Range", "XP"},
     {"BACKGROUND", "BETWEEN_CROSSOVER_Glasses", "BETWEEN_CROSSOVER_Pants", "BETWEEN_CROSSOVER_Arms", "BETWEEN_CROSSOVER_Shoes", "BETWEEN_CROSSOVER_Shirt", "BETWEEN_CROSSOVER_Necklace", "BETWEEN_CROSSOVER_Move", "BETWEEN_CROSSOVER_Ball", "Range", "XP"},
     {"BACKGROUND", "HESITATION_Glasses", "HESITATION_Pants", "HESITATION_Arms", "HESITATION_Shoes", "HESITATION_Shirt", "HESITATION_Necklace", "HESITATION_Move", "HESITATION_Ball", "Range", "XP"},
-    {"BACKGROUND", "IN_N_OUT_Glasses", "IN_N_OUT_Pants", "IN_N_OUT_Arms", "IN_N_OUT_Shoes", "IN_N_OUT_Shirt", "IN_N_OUT_Necklace", "IN_N_OUT_Move", "IN_N_OUT_Ball", "Range", "XP"}
-   
+    {"BACKGROUND", "IN_N_OUT_Glasses", "IN_N_OUT_Pants", "IN_N_OUT_Arms", "IN_N_OUT_Shoes", "IN_N_OUT_Shirt", "IN_N_OUT_Necklace", "IN_N_OUT_Move", "IN_N_OUT_Ball", "Range", "XP"},
+    {"BACKGROUND", "SPIN_MOVE_Glasses", "SPIN_MOVE_Pants", "SPIN_MOVE_Arms", "SPIN_MOVE_Shoes", "SPIN_MOVE_Shirt", "SPIN_MOVE_Necklace", "SPIN_MOVE_Move", "SPIN_MOVE_Ball", "Range", "XP"},
+    {"BACKGROUND", "CROSSOVER_Glasses", "CROSSOVER_Pants", "CROSSOVER_Arms", "CROSSOVER_Shoes", "CROSSOVER_Shirt", "CROSSOVER_Necklace", "CROSSOVER_Move", "CROSSOVER_Ball", "Range", "XP"},
+    {"BACKGROUND", "BETWEEN_THE_LEGS_Glasses", "BETWEEN_THE_LEGS_Pants", "BETWEEN_THE_LEGS_Arms", "BETWEEN_THE_LEGS_Shoes", "BETWEEN_THE_LEGS_Shirt", "BETWEEN_THE_LEGS_Necklace", "BETWEEN_THE_LEGS_Move", "BETWEEN_THE_LEGS_Ball", "Range", "XP"},
+    {"BACKGROUND", "BEHIND_THE_BACK_Glasses", "BEHIND_THE_BACK_Pants", "BEHIND_THE_BACK_Arms", "BEHIND_THE_BACK_Shoes", "BEHIND_THE_BACK_Shirt", "BEHIND_THE_BACK_Necklace", "BEHIND_THE_BACK_Move", "BEHIND_THE_BACK_Ball", "Range", "XP"}
+    
         };
         
         const string name = "Baller Bag Handles Package";
@@ -75,6 +76,7 @@ int main(int argc, char *argv[])
         convertCollectionSize(collectionSize); 
         randomIndex.resize(collectionSize[(int)collectionSize.size()-1]);
         dnaOfAllMedia.resize(collectionSize[(int)collectionSize.size()-1]);
+        addEmptyTrackAudioToVideos();
         if(argc==2){
             if((int)collectionSize.size()>=1){
                 deleteAllFilesOfFolder("./tmp");
@@ -93,7 +95,7 @@ int main(int argc, char *argv[])
                 deleteCharactersFromDnas(dnaOfAllMedia);
                 for(int i=1;i<(int)collectionSize.size();++i){
                     if(chain=="SOL")
-                        generateAllMediaSolanaMetadata(layerDir[i-1], singleLayer[i-1], name, description, symbol, family, sellerFeeBasisPoints, externalUrl, address, share, extraMetadata, collectionSize, i, dnaOfAllMedia, outputFormat);
+                        generateAllMediaSolanaMetadata(layerDir[i-1], metadataSingleLayerName[i-1], name, description, symbol, family, sellerFeeBasisPoints, externalUrl, address, share, extraMetadata, collectionSize, i, dnaOfAllMedia, outputFormat);
                     else
                         generateAllMediaMetadata(layerDir[i-1], metadataSingleLayerName[i-1], name, description, baseUri, extraMetadata, collectionSize, i, dnaOfAllMedia, outputFormat);
                     generateAllMedia(layerDir[i-1], singleLayer[i-1], collectionSize, i, scale, dnaOfAllMedia, outputFormat, chain);
@@ -118,7 +120,7 @@ int main(int argc, char *argv[])
                 for(int i=1;i<(int)collectionSize.size();++i){
                 readLayersAndRaritys(layerDir[i-1], singleLayer[i-1], metadataSingleLayerName[i-1], rarityWeight[i-1]);
                     if(chain=="SOL")
-                        generateAllMediaSolanaMetadata(layerDir[i-1], singleLayer[i-1], name, description, symbol, family, sellerFeeBasisPoints, externalUrl, address, share, extraMetadata, collectionSize, i, dnaOfAllMedia, outputFormat);
+                        generateAllMediaSolanaMetadata(layerDir[i-1], metadataSingleLayerName[i-1], name, description, symbol, family, sellerFeeBasisPoints, externalUrl, address, share, extraMetadata, collectionSize, i, dnaOfAllMedia, outputFormat);
                     else
                         generateAllMediaMetadata(layerDir[i-1], metadataSingleLayerName[i-1], name, description, baseUri, extraMetadata, collectionSize, i, dnaOfAllMedia, outputFormat);
                 }
@@ -133,11 +135,19 @@ int main(int argc, char *argv[])
                 generatePreviewGif(firstMedia, numbOfMediaToGen, scale, fps);
             else if(utility=="combine_video_with_audio")
                 mergeAllAudioWithVideo(firstMedia, inputVideoForm, inputAudioForm, outputVideoForm, numbOfMediaToGen);
-            else
+            else{
                 cerr << "parameters invalid, check your input" << endl;
+                
+            }
         }
     }
     else
         cerr << "number of parameters invalid, check your input" << endl;
  return 0;
 }
+
+/*
+    exit(1) -> for ffmpeg error
+    exit(2) -> for reading / writing operation from file
+    exit(3) -> for some user input error
+*/
